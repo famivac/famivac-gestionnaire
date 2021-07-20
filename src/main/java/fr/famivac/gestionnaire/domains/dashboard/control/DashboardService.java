6fd11e13a74b37851386c3bd555e7787f1c1e908
Paolo Escobar <paolo@solidstategroup.com>
2021-07-20 19:39:11 +0100

    Prettier

--- src/main/java/fr/famivac/gestionnaire/domains/dashboard/control/DashboardService.java
+++ src/main/java/fr/famivac/gestionnaire/domains/dashboard/control/DashboardService.java
@@ -1,8 +1,8 @@
 package fr.famivac.gestionnaire.domains.dashboard.control;
 
 import fr.famivac.gestionnaire.domains.enfants.entity.EnfantRepository;
-import fr.famivac.gestionnaire.domains.familles.entity.FamilleRepository;
 import fr.famivac.gestionnaire.domains.enfants.entity.InscripteurRepository;
+import fr.famivac.gestionnaire.domains.familles.entity.FamilleRepository;
 import fr.famivac.gestionnaire.domains.sejours.entity.SejourRepository;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
@@ -11,30 +11,23 @@ import javax.inject.Inject;
 import javax.interceptor.Interceptors;
 import net.bull.javamelody.MonitoringInterceptor;
 
-/**
- * @author paoesco
- */
+/** @author paoesco */
 @Stateless
 @Interceptors({MonitoringInterceptor.class})
 @TransactionAttribute(TransactionAttributeType.NEVER)
 public class DashboardService {
-    
-    @Inject
-    private FamilleRepository familleRepository;
-    @Inject
-    private EnfantRepository enfantRepository;
-    @Inject
-    private SejourRepository sejourRepository;
-    @Inject
-    private InscripteurRepository inscripteurRepository;
-    
-    public DashboardDTO getDefault() {
-        DashboardDTO dashboardDTO = new DashboardDTO();
-        dashboardDTO.setNombreFamilles(familleRepository.countActives());
-        dashboardDTO.setNombreEnfants(enfantRepository.count());
-        dashboardDTO.setNombreInscripteurs(inscripteurRepository.count());
-        dashboardDTO.setNombreSejoursEnCours(sejourRepository.countActifs());
-        return dashboardDTO;
-    }
-    
+
+  @Inject private FamilleRepository familleRepository;
+  @Inject private EnfantRepository enfantRepository;
+  @Inject private SejourRepository sejourRepository;
+  @Inject private InscripteurRepository inscripteurRepository;
+
+  public DashboardDTO getDefault() {
+    DashboardDTO dashboardDTO = new DashboardDTO();
+    dashboardDTO.setNombreFamilles(familleRepository.countActives());
+    dashboardDTO.setNombreEnfants(enfantRepository.count());
+    dashboardDTO.setNombreInscripteurs(inscripteurRepository.count());
+    dashboardDTO.setNombreSejoursEnCours(sejourRepository.countActifs());
+    return dashboardDTO;
+  }
 }
