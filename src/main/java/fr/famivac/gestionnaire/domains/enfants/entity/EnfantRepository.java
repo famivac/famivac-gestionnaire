b3845bdbb2b5969b178b3ef273072c3e671b36ae
Paolo <paolo@konfir.com>
2023-01-02 16:55:58 +0000

    Fixes startup & README

--- src/main/java/fr/famivac/gestionnaire/domains/enfants/entity/EnfantRepository.java
+++ src/main/java/fr/famivac/gestionnaire/domains/enfants/entity/EnfantRepository.java
@@ -9,37 +9,38 @@ import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
 /**
- *
  * @author paoesco
  */
 @ApplicationScoped
 public class EnfantRepository {
 
-    @Inject
-    private EntityManager entityManager;
+  @Inject
+  private EntityManager entityManager;
 
-    public List<EnfantListView> search(String nomEnfant, String prenomEnfant) {
-        Query query = entityManager.createQuery("""
-            select new fr.famivac.gestionnaire.domains.enfants.entity.views.EnfantListView(
-                e.id,
-                e.nom,
-                e.prenom
-            )
-            from Enfant e
-            where lower(e.nom) like :nom
-            and lower(e.prenom) like :prenom 
-            order by e.nom
-        """, EnfantListView.class);
-        query.setParameter("nom", "%" + (Objects.isNull(nomEnfant) ? "" : nomEnfant.toLowerCase() + "%"));
-        query.setParameter("prenom", "%" + (Objects.isNull(prenomEnfant) ? "" : prenomEnfant.toLowerCase() + "%"));
-        return query.getResultList();
-    }
+  public List<EnfantListView> search(String nomEnfant, String prenomEnfant) {
+    Query query = entityManager.createQuery(
+        "select new fr.famivac.gestionnaire.domains.enfants.entity.views.EnfantListView(" +
+            " e.id," +
+            " e.nom," +
+            " e.prenom" +
+            " )" +
+            " from Enfant e" +
+            " where lower(e.nom) like :nom" +
+            " and lower(e.prenom) like :prenom" +
+            " order by e.nom"
+        , EnfantListView.class);
+    query.setParameter("nom",
+        "%" + (Objects.isNull(nomEnfant) ? "" : nomEnfant.toLowerCase() + "%"));
+    query.setParameter("prenom",
+        "%" + (Objects.isNull(prenomEnfant) ? "" : prenomEnfant.toLowerCase() + "%"));
+    return query.getResultList();
+  }
 
-    public Long count() {
-        String jpql = "select count(e.id) from Enfant e";
-        Query q = entityManager.createQuery(jpql);
-        return (long) q.getSingleResult();
-    }
+  public Long count() {
+    String jpql = "select count(e.id) from Enfant e";
+    Query q = entityManager.createQuery(jpql);
+    return (long) q.getSingleResult();
+  }
 
 //    private String stripAccents(String s) {
 //        s = Normalizer.normalize(s, Normalizer.Form.NFD);
